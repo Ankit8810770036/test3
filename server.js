3de448a5c8b84bc63af638595e63253664b62bbb
[Ankit kumar singh] <[ankitkrsingh210@gmail.com]>
2026-06-04 00:08:58 +0530

    Fix server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins Automation Demo!');
+  res.send('Hello from Jenkins Automation Demo! with second change');
 });
 
 app.listen(port, () => {
